2fdaa4d03a9714ca10984f7c13456a75b212db37
Joao Pedro Santos <jpsantos.wj@gmail.com>
2023-08-09 11:39:25 -0300

    feat: add lorem

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -18,11 +18,11 @@ export default async function IndexPage() {
             Acompanhe no Twitter
           </Link>
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            Consultoria e treinamento de Neurologia
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            Realizamos atendimento de forma presencial ou remota, conforma a
-            necessidade de cada um.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+            vel asperiores atque aspernatur quas fugiat quos voluptate?
           </p>
           <div className="space-x-4">
             <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
@@ -66,11 +66,12 @@ export default async function IndexPage() {
             <div className="flex flex-col justify-between rounded-md p-6">
               <Icons.brain className="h-12 w-12 mb-5"></Icons.brain>
               <div className="space-y-2">
-                <h3 className="font-bold">Avaliação Neuropsicológica</h3>
+                <h3 className="font-bold">Lorem ipsum dolor sit amet</h3>
                 <p className="text-sm text-muted-foreground">
-                  A Avaliação Neuropsicológica é o exame detalhado das funções
-                  cognitivas através de um conjunto de testes neuropsicológicos,
-                  escalas e observação comportamental.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quasi ea deserunt nihil aliquam eos quos repellat harum
+                  officia, labore repellendus esse nemo error porro cum corrupti
+                  quod accusantium rerum? Itaque.
                 </p>
               </div>
             </div>
@@ -80,11 +81,12 @@ export default async function IndexPage() {
               <Icons.book className="h-12 w-12 mb-5"></Icons.book>
               <div className="space-y-2">
                 <h3 className="font-bold">
-                  Orientação Profissional (Vocacional)
+                  Lorem ipsum dolor sit aliquam (amet)
                 </h3>
                 <p className="text-sm text-muted-foreground">
-                  A orientação vocacional é um serviço que visa auxiliar no
-                  processo de escolha profissional.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quasi ea deserunt nihil aliquam eos quos repellat harum
+                  officia
                 </p>
               </div>
             </div>
@@ -93,14 +95,12 @@ export default async function IndexPage() {
             <div className="flex flex-col justify-between rounded-md p-6">
               <Icons.heart className="h-12 w-12 mb-5"></Icons.heart>
               <div className="space-y-2">
-                <h3 className="font-bold">
-                  Avaliação Psicológica para Cirurgia Neurológica
-                </h3>
+                <h3 className="font-bold">Lorem ipsum dolor sit amet</h3>
                 <p className="text-sm text-muted-foreground">
-                  A avaliação psicológica pode certificar de que o paciente
-                  possui nível intelectual e cognitivo de compreensão acerca dos
-                  riscos da operação e cuidados inerentes a esse procedimento no
-                  período do pós-operatório imediato e em longo prazo.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quasi ea deserunt nihil aliquam eos quos repellat harum
+                  officia, labore repellendus esse nemo error porro cum corrupti
+                  quod accusantium rerum? Itaque.
                 </p>
               </div>
             </div>
@@ -109,13 +109,12 @@ export default async function IndexPage() {
             <div className="flex flex-col justify-between rounded-md p-6">
               <Icons.messagesSquare className="h-12 w-12 mb-5"></Icons.messagesSquare>
               <div className="space-y-2">
-                <h3 className="font-bold">
-                  Terapia Cognitivo-comportamental (TCC)
-                </h3>
+                <h3 className="font-bold">Lorem ipsum dolor sit amet</h3>
                 <p className="text-sm text-muted-foreground">
-                  Psicoterapia breve estruturada, orientada ao presente,
-                  direcionada a resolver problemas do presente e modificar
-                  pensamentos e/ou comportamentos disfuncionais.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quasi ea deserunt nihil aliquam eos quos repellat harum
+                  officia, labore repellendus esse nemo error porro cum corrupti
+                  quod accusantium rerum? Itaque.
                 </p>
               </div>
             </div>
@@ -124,12 +123,9 @@ export default async function IndexPage() {
             <div className="flex flex-col justify-between rounded-md p-6">
               <Icons.help className="h-12 w-12 mb-5"></Icons.help>
               <div className="space-y-2">
-                <h3 className="font-bold">Estimulação Cognitiva</h3>
+                <h3 className="font-bold">Lorem ipsum dolor sit amet</h3>
                 <p className="text-sm text-muted-foreground">
-                  Visa trabalhar as dificuldades apresentadas por crianças, tais
-                  como problemas de memória, atenção, planejamento, raciocínio
-                  lento, linguagem através do treino cognitivo e reconstrução de
-                  atividades da vida diária do paciente.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 </p>
               </div>
             </div>
@@ -138,12 +134,11 @@ export default async function IndexPage() {
             <div className="flex flex-col justify-between rounded-md p-6">
               <Icons.users className="h-12 w-12 mb-5"></Icons.users>
               <div className="space-y-2">
-                <h3 className="font-bold">Reabilitação Neuropsicológica</h3>
+                <h3 className="font-bold">Lorem ipsum dolor sit amet</h3>
                 <p className="text-sm text-muted-foreground">
-                  Melhorar a qualidade de vida e a capacidade cognitiva de
-                  clientes com lesão cerebral ou em déficit/ declínio cognitivo,
-                  promovendo a readaptação aos níveis sociais, ocupacionais e
-                  psíquicos do paciente à doença e déficits cognitivos causados.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quasi ea deserunt nihil aliquam eos quos repellat harum
+                  officia
                 </p>
               </div>
             </div>
